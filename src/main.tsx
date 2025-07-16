d102c4a980c86cff7387502b8c4f00ae8583fb46
Jon Underwood <junderwood@Mac.attlocal.net>
2025-07-15 20:16:01 -0500

    added additional routing for gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css'; // Make sure you import your main CSS file
+import { BrowserRouter } from 'react-router-dom';
+
+const basename = import.meta.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={basename}> {/* <--- Add basename here */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
