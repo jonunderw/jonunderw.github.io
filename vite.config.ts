6c90de63f41380169c11c30894a2b3e9e4c4e374
Jon Underwood <junderwood@Mac.attlocal.net>
2025-08-17 19:08:32 -0500

    reverted changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/jonunderw.github.io/',
+  base: '/',
   assetsInclude: ['**/*.md'],
   define: {
     global: 'globalThis',
